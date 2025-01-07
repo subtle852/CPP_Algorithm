3a1171506cdfef42df085b97e4237b55d2e09f4d
subtle852 <subtle852@gmail.com>
2025-01-08 04:13:50 +0900

    feat: 큐 구현 (연결리스트 방식, 동적배열 방식)

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -5,6 +5,8 @@
 #include "MyList.h"
 #include <stack>
 #include "MyStack.h"
+#include <queue>
+#include "MyQueue.h"
 using namespace std;
 
 
@@ -59,26 +61,44 @@ int main()
 	}
 
 	{
-		//stack<int> s;
-		//stack<int, deque<int>> s;
-		//stack<int, vector<int>> s;
-		//stack<int, list<int>> s;
-		MyStack<int, list<int>> s;
+		////stack<int> s;
+		////stack<int, deque<int>> s;
+		////stack<int, vector<int>> s;
+		////stack<int, list<int>> s;
+		//MyStack<int, list<int>> s;
 
-		// 삽입
-		s.push(1);
-		s.push(2);
-		s.push(3);
+		//// 삽입
+		//s.push(1);
+		//s.push(2);
+		//s.push(3);
 
-		while (s.empty() == false)
+		//while (s.empty() == false)
+		//{
+		//	// 최상위 원소 삭제
+		//	int data = s.top();
+		//	s.pop();
+
+		//	cout << data << endl;
+		//}
+
+		//int size = s.size();
+	}
+
+	{
+		//queue<int> q;
+		MyVectorQueue<int> q;
+		//MyListQueue<int> q;
+
+		for (int i = 0; i < 100; i++)
+			q.push(i);
+
+		while (q.empty() == false)
 		{
-			// 최상위 원소 삭제
-			int data = s.top();
-			s.pop();
-
-			cout << data << endl;
+			int value = q.front();
+			q.pop();
+			cout << value << endl;
 		}
 
-		int size = s.size();
+		int size = q.size();
 	}
 }
